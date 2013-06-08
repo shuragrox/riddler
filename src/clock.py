8da8c6994cdd91ac1df18467bc743d374e940d52
Nahuel Benítez <beniteznahueloscar@gmail.com>
2013-06-08 14:01:59 -0300

    Modified some mistakes

--- src/clock.py
+++ src/clock.py
@@ -1,14 +1,14 @@
-from threading import threading
+import threading
 import time
 
-class Clock(Thread):
+class Clock(threading.Thread):
 
     def __init__(self, aTimer):
-		Thread.__init__(self)
+		threading.Thread.__init__(self)
 		self.timer = aTimer
 
     def run(self):
-    """Sleeps one second, and then execute an instruction on the CPU."""
+        """Sleeps one second, and then clocks the timer."""
         while(True):
      	    time.sleep(1)
             self.timer.clock()
