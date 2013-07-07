6c03233c3d0d3d9e44dd0aedb3a162c796368e96
shuragrox <beniteznahueloscar@gmail.com>
2013-07-06 21:35:32 -0300

    Fixed mmu & pcb

--- src/pcb.py
+++ src/pcb.py
@@ -1,26 +1,34 @@
 class PCB():
-    def __init__(self, aPid):
+    def __init__(self, aPid, aBase, aSize):
         self.pid = aPid
         self.pc = 0
+        self.base = aBase
+        self.size = aSize
 
     def getPc(self):
         return self.pc
 
-    def setPc(self, aNumber):
-        self.pc = aNumber
+    def size():
+        return this.size
+
+    def getSize():
+        return this.size
 
     def incPc(self):
         """"Increases the pc by 1"""
         self.pc += 1
 
-    def getID(self):
+    def getID(selflf):
         return self.pid
+
+    def getBase(self):
+        return self.base
 
     def hasNextInstruction(self, aSize):
         return (self.pc < aSize) and (self.pc != aSize)
 
 class PCBPriority(PCB):
-    def __init__(self, aPid, aPriority):
-        PCB.__init__(self,aPid)
+    def __init__(self, aPid, aBase, aPriority):
+        PCB.__init__(self, aPid, aBase)
         self.priority = aPriority
 
